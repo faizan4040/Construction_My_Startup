b2407e1bbc097c07fd6f85fd8e6c7e32447d92b7
Faizan Saifi <Faizansaifi571@gmail.com>
2026-09-01 12:07:25 +0530

    new_changes_222222222

--- routes/ShopOwnerPanelRoute.js
+++ routes/ShopOwnerPanelRoute.js
@@ -4,7 +4,7 @@ export const SHOP_OWNER_DASHBOARD = '/shop/dashboard'
 export const SHOP_OWNER_PRODUCT_ADD = '/shop/product/add'
 export const SHOP_OWNER_PRODUCT_SHOW = '/shop/product'
 
-// export const SHOP_OWNER_CATEGORY_ADD = '/shop/category/add'
+export const SHOP_OWNER_CATEGORY_ADD = '/shop/category/add'
 export const SHOP_OWNER_CATEGORY_SHOW = '/shop/category'
 export const SHOP_OWNER_ORDER_SHOW = '/shop/orders'
 
